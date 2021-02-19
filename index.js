86631143e4d5d15053670311c8035d2f182591cd
Trayshawn <traytwebb14@yahoo.com>
2021-02-19 12:45:21 -0600

    commit tree 8a8330a1af9a821fa2c2b81f762eed5ff1276f98

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Add your code here
+function submitData(userName, userEmail) {
+    const userData = {
+        name: userName,
+        email: userEmail
+    };
+
+    const configObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(userData)
+    };
+
+    return fetch("http://localhost:3000/users", configObj).then(function(response){ 
+        return response.json();
+        })
+        .then(function(object){
+            postIdToDom(object["id"])
+        })
+        .catch(function(error) {
+            document.body.append(error.message);
+        }); 
+ 
+
+};
+
+
+
+function postIdToDom(id) {
+    if (!document.getElementById("listOfUserIds")) {
+    let ul = document.createElement('ul')
+    ul.id = "listOfUserIds"
+    document.body.append(ul)
+    }
+
+    let listOfUserIds = document.getElementById("listOfUserIds")
+    let li = document.createElement('li')
+    li.innerHTML = id 
+    listOfUserIds.appendChild(li)
+
+}
